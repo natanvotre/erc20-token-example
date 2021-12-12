5d6ac166cdcb0006aea06a97f31c0535957f1890
Natan Votre <nvt@brickabode.com>
2021-12-12 19:11:36 -0300

    Add KycContract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,4 +1,5 @@
 var MyToken = artifacts.require("./MyToken.sol");
+var KycContract = artifacts.require("./KycContract.sol");
 var MyTokenSale = artifacts.require("./MyTokenSale.sol");
 require("dotenv").config({path: "../.env"})
 
@@ -6,7 +7,8 @@ module.exports = async function(deployer) {
   initialSupply = process.env.INITIAL_SUPPLY;
   let addresses = await web3.eth.getAccounts();
   await deployer.deploy(MyToken, initialSupply);
-  await deployer.deploy(MyTokenSale, 1, addresses[0], MyToken.address);
+  await deployer.deploy(KycContract);
+  await deployer.deploy(MyTokenSale, 1, addresses[0], MyToken.address, KycContract.adress);
 
   let instance = await MyToken.deployed();
   await instance.transfer(MyTokenSale.address, initialSupply);
