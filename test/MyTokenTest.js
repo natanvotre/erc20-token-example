947e97b416b02853533663eb761dc958fadb7045
Natan Votre <nvt@brickabode.com>
2021-12-12 18:08:10 -0300

    Add tests using chai-as-promised

---
+++ test/MyTokenTest.js
@@ -0,0 +1,44 @@
+const Token = artifacts.require("MyToken");
+
+var chai = require("chai");
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract("Token Test", async (accounts) => {
+
+    const [deployerAccount, recipientAccount, anotherAccount] = accounts;
+
+    beforeEach(async() => {
+        this.myToken = await Token.new(1000000);
+    });
+
+    it("all tokens should be in my account", async () => {
+        let instance = this.myToken;
+        let totalSupply = await instance.totalSupply();
+        expect(await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(totalSupply);
+    })
+
+    it("is possible to send tokens between accounts", async () => {
+        const nTokens = 1;
+        let instance = this.myToken;
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect(instance.transfer(recipientAccount, nTokens)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(nTokens)));
+        expect(instance.balanceOf(recipientAccount)).to.eventually.be.a.bignumber.equal(new BN(nTokens));
+    })
+
+    it("is not possible to send more tokens than avaiable", async () => {
+        let instance = this.myToken;
+        let totalSupply = await instance.totalSupply();
+        let beforeBalance = await instance.balanceOf(deployerAccount);
+        await expect(instance.transfer(recipientAccount, totalSupply+1)).to.eventually.be.rejected;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(beforeBalance);
+    })
+})
