b99ced809848731e0fd181dd67b1b926b7ce7c34
Natan Votre <nvt@brickabode.com>
2021-12-12 18:08:39 -0300

    Add CrowdSale to handle the ERC20 token

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.7.6;
+
+import "./CrowdSale.sol";
+
+contract MyTokenSale is Crowdsale {
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+}
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,12 @@
 var MyToken = artifacts.require("./MyToken.sol");
+var MyTokenSale = artifacts.require("./MyTokenSale.sol");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(MyToken, 10000000);
+  initialSupply = 10000000;
+  let addresses = await web3.eth.getAccounts();
+  await deployer.deploy(MyToken, initialSupply);
+  await deployer.deploy(MyToken, 1, addresses[0], MyToken.address);
+
+  let instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, initialSupply);
 };
