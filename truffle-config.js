f9d9fa930bf1ef0d2a027ea365fd9a2f168ae548
Natan Votre <nvt@brickabode.com>
2021-12-18 19:05:05 -0300

    Adapt the frontend to update the whitelist

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,24 @@
 const path = require("path");
+require("dotenv").config({path: ".env"})
+
+const HdWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
+    development: {
+      host: "localhost",
+      network_id: 5777,
       port: 8545
+    },
+    ganache_local: {
+      provider: function () {
+        return new HdWalletProvider(process.env.WALLET_MNEMONIC, "http://localhost:8545", AccountIndex)
+      },
+      network_id: 5777
     }
   },
   compilers: {
